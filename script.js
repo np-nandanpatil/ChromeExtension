6596a332948f6d5cf5814ca0f01fa79a7eb59b17
np <nandanpatilm15@gmail.com>
2025-01-05 23:15:56 +0530

    Done with the web page!

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const quotes = [ 
+     "Be consistent while coding", 
+     "Remember that you have 1% chance!", 
+     "The longer you take time to achieve your goals, the more you have financial instability", 
+]
 
+function getQuote(){
+     return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+     const quote = document.getElementById('quote');
+     quote.textContent = getQuote();
+});
