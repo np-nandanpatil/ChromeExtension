3a2810825a6477c5ade5a93b35da4907252c429d
np <nandanpatilm15@gmail.com>
2025-01-06 13:23:26 +0530

    Added remainng time quote.

--- script.js
+++ script.js
@@ -2,25 +2,32 @@ const target = new Date('2025-03-30')
 
 function getRemDate(){
      const today = new Date()
-     const rem = target - today
-     return Math.ceil(rem / (1000 * 60 * 60 * 24))
+     const remDays = target - today
+     
+     return Math.ceil(remDays / (1000 * 60 * 60 * 24))
 }
 
+function getRemTime(){
+     const timeNow = new Date()
+     const hours = 23 - timeNow.getHours()
+     const minutes = 59 - timeNow.getMinutes()
+     
+     return '${hours}h ${minutes}m left'
+}
 const quotes = [ 
      "Be consistent!", 
      "You have 1% chance!", 
-     "The longer you take time to achieve your goals, the more you have financial instability",
      "Focus on small wins.",
      "Have realistic goals.",
      "Plan your day.",
-     "Remember you have " + getRemDate() + " days left."
+     getRemDate() + " days left!"
 ]
 
 function getQuote(){
-     return quotes[Math.floor(Math.random() * quotes.length)];
+     return quotes[Math.floor(Math.random() * quotes.length)]
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-     const quote = document.getElementById('quote');
-     quote.textContent = getQuote();
+     const quote = document.getElementById('quote')
+     quote.textContent = getQuote()
 });
